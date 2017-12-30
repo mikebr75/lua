0f49bf92bc459891206668864df8428fc7f08751
Mike Brenner <mikebrenner75@outlook.com>
2017-12-30 16:49:47 -0500

    fixed syntax and cleaned

--- logger_on_gpio_0.lua
+++ logger_on_gpio_0.lua
@@ -1,13 +1,13 @@
---main call of logger
+--check logger on schedule
 function onTick()
    checkLoggerState()
 end
 
 --start the logger if not already logging
 function checkLoggerState()
-   if(getGpio(0) == 1 && isLogging() == 0) then
+   if getGpio(0) == 1 and isLogging() == 0 then
       startLogging()
-   else if(getGpio(0) == 0 && isLogging() ==1)
+   elseif getGpio(0) == 0 and isLogging() == 1
       stopLogging()
    end   
 end
